8ef4467671c40d62f5b50b061ce8782f7a1acb82
Briana <briana_brewster4@yahoo.com>
2022-10-24 14:26:05 -0500

    updated NavBar

--- phase-3-project-frontend/src/components/NavBar.js
+++ phase-3-project-frontend/src/components/NavBar.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
     return(
-        <div>
-
-        </div>
+        <nav>
+            <NavLink exact to="/">Home</NavLink>
+            <NavLink to="/dogs">Dogs</NavLink>
+            <NavLink to="/walkers">Walkers</NavLink>
+            <NavLink to="/application">Application</NavLink>
+            <NavLink to="/appointments">Appointments</NavLink>
+        </nav>
     )
 }
 
