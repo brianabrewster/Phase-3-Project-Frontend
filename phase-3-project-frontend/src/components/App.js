ed9344a4bd02bf614daa96a376ed36d6590c5623
Tyler Hall <thall2980@gmail.com>
2022-10-24 14:59:48 -0400

    routing and app layout

--- phase-3-project-frontend/src/components/App.js
+++ phase-3-project-frontend/src/components/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/dogs">
+            <Dogs />
+          </Route>
+          <Route path="/walkers">
+            <Walkers />
+          </Route>
+          <Route path="/application">
+            <Application />
+          </Route>
+          <Route path="/appointments">
+            <Appointments />
+          </Route>
+        </Switch>
       </header>
     </div>
   );
