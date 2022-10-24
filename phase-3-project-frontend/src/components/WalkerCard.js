675ba1488d04617c713e49071302faa379f51938
Briana <briana_brewster4@yahoo.com>
2022-10-24 15:09:01 -0500

    updated WalkCard, Walkers, and NavBar

---
+++ phase-3-project-frontend/src/components/WalkerCard.js
@@ -0,0 +1,13 @@
+function WalkerCard() {
+    return(
+        <li className="card">
+         <div className="card__content">
+             <h1 className="card__title">Walker's Name Here</h1>
+             <p>Years of Experience: {}</p>
+             <p>Pace: {}</p>
+         </div>
+         </li>  
+     )
+}
+
+export default WalkerCard;
--- phase-3-project-frontend/src/components/Walkers.js
+++ phase-3-project-frontend/src/components/Walkers.js
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from "react";
+import WalkerCard from "./WalkerCard";
+
 function Walkers() {
+    const [walkers, setWalkers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/walkers')
+        .then((res) => res.json())
+        .then((data) => setWalkers(data))
+    }, [])
+
+    const renderWalkers = walkers.map((walker) => {
+        return <WalkerCard {...walker} key={walker.id}/>
+    })
+
     return(
         <div>
-
+            {renderWalkers}
         </div>
     )
 }
