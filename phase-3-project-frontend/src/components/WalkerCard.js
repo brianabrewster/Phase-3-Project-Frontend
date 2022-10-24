cfa1a31abaf895074496efa75e2b5584330fc0bd
Briana <briana_brewster4@yahoo.com>
2022-10-24 15:19:25 -0500

    updated walker card component

--- phase-3-project-frontend/src/components/WalkerCard.js
+++ phase-3-project-frontend/src/components/WalkerCard.js
@@ -1,10 +1,11 @@
-function WalkerCard() {
+function WalkerCard({ name, years_of_experience, pace, image }) {
     return(
         <li className="card">
          <div className="card__content">
-             <h1 className="card__title">Walker's Name Here</h1>
-             <p>Years of Experience: {}</p>
-             <p>Pace: {}</p>
+             <h1 className="card__title">{name}</h1>
+             <img src={image} alt="walker_image"></img>
+             <p>Years of Experience: {years_of_experience}</p>
+             <p>Pace: {pace}</p>
          </div>
          </li>  
      )
