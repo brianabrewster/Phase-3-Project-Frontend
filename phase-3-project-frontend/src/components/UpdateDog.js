ecdb0ae2ea9e9fce13811e6463c70c2f347bfaea
Tyler Hall <thall2980@gmail.com>
2022-10-26 15:26:28 -0400

    FINAL?

--- phase-3-project-frontend/src/components/UpdateDog.js
+++ phase-3-project-frontend/src/components/UpdateDog.js
@@ -33,42 +33,42 @@ const UpdateDog = ({ dog, onUpdateDog, handleCancel, setIsMainInfo }) => {
       <form onSubmit={handleSubmit} className="updateForm">
         <label className="updateLabel">Name</label>
         <input
-          className="formInput"
+          className="updateFormInput"
           value={name}
           type="text"
           onChange={(e) => setName(e.target.value)}
         ></input>
         <label className="updateLabel">Owner</label>
         <input
-          className="formInput"
+          className="updateFormInput"
           value={owner}
           type="text"
           onChange={(e) => setOwner(e.target.value)}
         ></input>
         <label className="updateLabel">Breed</label>
         <input
-          className="formInput"
+          className="updateFormInput"
           value={breed}
           type="text"
           onChange={(e) => setBreed(e.target.value)}
         ></input>
         <label className="updateLabel">Temperament</label>
         <input
-          className="formInput"
+          className="updateFormInput"
           value={temperament}
           type="text"
           onChange={(e) => setTemperament(e.target.value)}
         ></input>
         <label className="updateLabel">Age</label>
         <input
-          className="formInput"
+          className="updateFormInput"
           value={age}
           type="integer"
           onChange={(e) => setAge(e.target.value)}
         ></input>
         <label className="updateLabel">Image</label>
         <input
-          className="formInput"
+          className="updateFormInput"
           value={image}
           type="text"
           onChange={(e) => setImage(e.target.value)}
