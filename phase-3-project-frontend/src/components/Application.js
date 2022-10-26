da23d2b5bae382a96d3129016467a43b0314efcf
Tyler Hall <thall2980@gmail.com>
2022-10-26 10:45:18 -0400

    app form css

--- phase-3-project-frontend/src/components/Application.js
+++ phase-3-project-frontend/src/components/Application.js
@@ -43,48 +43,60 @@ function Application({ onAddDog }) {
     <div class="applicationFormContainer">
       <h1 class="applicationTitle">Register a Pet</h1>
       <form class="applicationForm" onSubmit={handleSubmit}>
-        <label>Name</label>
+        <label className="appLabel">Name</label>
         <input
+          className="formInput"
           value={name}
           type="text"
           placeholder="Name..."
           onChange={(e) => setName(e.target.value)}
         ></input>
-        <label>Owner</label>
+        <br />
+        <label className="appLabel">Owner</label>
         <input
+          className="formInput"
           value={owner}
           type="text"
           placeholder="Owner..."
           onChange={(e) => setOwner(e.target.value)}
         ></input>
-        <label>Breed</label>
+        <br />
+        <label className="appLabel">Breed</label>
         <input
+          className="formInput"
           value={breed}
           type="text"
           placeholder="Breed..."
           onChange={(e) => setBreed(e.target.value)}
         ></input>
-        <label>Temperament</label>
+        <br />
+        <label className="appLabel">Temperament</label>
         <input
+          className="formInput"
           value={temperament}
           type="text"
           placeholder="Temperament..."
           onChange={(e) => setTemperament(e.target.value)}
         ></input>
-        <label>Age</label>
+        <br />
+        <label className="appLabel">Age</label>
         <input
+          className="formInput"
           value={age}
           type="integer"
           placeholder="Age... (Numbers Only)"
           onChange={(e) => setAge(e.target.value)}
         ></input>
-        <label>Picture</label>
+        <br />
+        <label className="appLabel">Picture</label>
         <input
+          className="formInput"
           value={image}
           type="text"
           placeholder="Image URL..."
           onChange={(e) => setImage(e.target.value)}
         ></input>
+        <br />
         <button id="appSubmit" type="submit">
           Register Dog
         </button>
